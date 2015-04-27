f3931da19b6b195cc41d594cb7a3a4f0f10309e1
mavey <Mavey@Maveys-MacBook-Pro.local>
2015-04-27 12:55:33 -0700

    Dragged entire Chinese Corner folder to mavey.github.io

---
+++ chinesecorner/assets/img/Chinese Corner - Discover Your Chinese Tongue_files/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+
+
+
+	
+})
+
+
+if $(".item").hover(function(){
+    $(".item").css("privatelessons_img", "#blurry");
+    }, else function(){
+    $(".item");
+});
